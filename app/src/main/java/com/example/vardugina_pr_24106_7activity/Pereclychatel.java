cdd050d5fae080e85f5f0d64a6852cf8216ab676
mobil <poll.vard.123@gmail.com>
2025-11-07 13:47:40 +0700

    пятая активити

---
+++ app/src/main/java/com/example/vardugina_pr_24106_7activity/Pereclychatel.java
@@ -0,0 +1,24 @@
+package com.example.vardugina_pr_24106_7activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Pereclychatel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pereclychatel);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
